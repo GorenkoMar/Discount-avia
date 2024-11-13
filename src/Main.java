22686659fe3560f5b9282ac71e1fc046a1802044
Gorenko Maria <kirillovsub@yandex.ru>
2024-11-13 12:11:14 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        int ticket_amount = 62140;
-        int bonus = 20;
-        int bonus_mile = ticket_amount / bonus;
-        System.out.println("Начислено бонусных миль:  " + bonus_mile);
-    }
+
+  public static void main(String[] args) {
+    int ticketAmount = 62140;
+    int bonus = 20;
+    int bonusMile = ticketAmount / bonus;
+    System.out.println("Начислено бонусных миль:  " + bonusMile);
+  }
 }
